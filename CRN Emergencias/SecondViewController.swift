4eb44cc8b5d1f8c19688d53010099830592538ea
Diego Trujillo Norberto <a01360477@dpdmaster-38.local>
2016-03-17 14:17:51 -0600

    Initial Commit

---
+++ CRN Emergencias/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CRN Emergencias
+//
+//  Created by Diego Trujillo Norberto on 17/03/16.
+//  Copyright © 2016 ITESM CEM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
