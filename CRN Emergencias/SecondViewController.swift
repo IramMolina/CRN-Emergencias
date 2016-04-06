2b62bf4da41ddc28e32cc68bd5f90ca9a74507e0
Andrea Pérez <a01373631@DPDMASTER-2.local>
2016-04-05 21:56:22 -0500

    venta table view emergencias

--- CRN Emergencias/SecondViewController.swift
+++ CRN Emergencias/SecondViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var table: UITableView!
+  
+    
+    let arrEmergencias = ["Alergias/Anafilaxia","Crisis asmática","Sangrado","Fracturas","Quemaduras","Atragantamiento/Asfixia","Emergencias Diabeticas", "Ataque de ansiedad","Lesiones en la cabeza","Infarto al corazón","Golpe de calor", "Hipotermia","Meningitis", "Envenenamiento/Sustancias peligrosas","Convulsiones/Epilepsia","Picadura/Mordedura","Torceduras/Esguince","Embolia cerebral","Inconsciente"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSectionsInTableView(tableView:	UITableView)	->	Int {
+        return	1
+    }
+    func tableView(tableView:	UITableView,	numberOfRowsInSection section:	Int)	->	Int {
+        return self.arrEmergencias.count
+    }
+    func tableView(tableView:	UITableView,	cellForRowAtIndexPath indexPath:	NSIndexPath)	->	UITableViewCell {
+        let celda = tableView.dequeueReusableCellWithIdentifier("celdaID", forIndexPath: indexPath)
+        celda.textLabel!.text =	self.arrEmergencias[indexPath.row]
+        return celda
+    }
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = .clearColor()
+        cell.textLabel?.textColor = UIColor.whiteColor()
+    }
+    
+    
 
 }
 
